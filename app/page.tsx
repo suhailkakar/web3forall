dee25a666080ff860dcce4fbd2982009e06a9440
Suhail Kakar <suhail.zahed@gmail.com>
2024-07-24 15:34:21 +0530

    seo: add og image for each item

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import DefaultPopup from "@/components/default-popup";
 import Hero from "@/components/hero";
+import { images } from "@/lib/constants";
 import Image from "next/image";
 import React from "react";
 
@@ -12,10 +13,29 @@ export default function Page({
 }) {
   const { id } = searchParams;
 
+  const defaultPopup = images.find((image) => image.id === Number(id)) as any;
+
   return (
-    <div className="">
+    <section>
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Website",
+            headline: id ? defaultPopup.name : "Web3 For All",
+            image: id ? defaultPopup.url : "https://web3forall.xyz/og.png",
+            url: `https://web3forall.xyz${id ? `?id=${id}` : ""}`,
+            author: {
+              "@type": "Person",
+              name: "Suhail Kakar",
+            },
+          }),
+        }}
+      />
       <Hero />
       {id && <DefaultPopup id={Number(id)} />}
-    </div>
+    </section>
   );
 }
