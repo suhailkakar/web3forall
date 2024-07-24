20ce8eb2632aa1bb00144197242cf80a57f2df8c
Suhail Kakar <suhail.zahed@gmail.com>
2024-07-24 18:24:00 +0530

    ui: update button color to monochrome

--- components/ui/hover-border-gradient.tsx
+++ components/ui/hover-border-gradient.tsx
@@ -45,7 +45,7 @@ export function HoverBorderGradient({
   };
 
   const highlight =
-    "radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)";
+    "radial-gradient(75% 181.15942028985506% at 50% 50%, #fff 0%, rgba(255, 255, 255, 0) 100%)";
 
   useEffect(() => {
     if (!hovered) {
