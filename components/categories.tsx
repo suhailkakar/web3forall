3c62a270e4c5cd04b78956511f0ca99ec2df32d2
Suhail Kakar <suhail.zahed@gmail.com>
2024-07-24 15:58:46 +0530

    feat: add search option

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { categories, images } from "@/lib/constants";
-import Image from "next/image";
+import { categories, images as allImages } from "@/lib/constants";
 import Tile from "./ui/tile";
+import { Input } from "./ui/input";
 
 export default function Categories({
   searchParams,
@@ -13,6 +15,14 @@ export default function Categories({
   };
 }) {
   const { category } = searchParams || { category: "all" };
+  const [images, setImages] = useState(allImages);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filteredImages = allImages.filter((image) =>
+      image.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setImages(filteredImages);
+  };
 
   return (
     <div>
@@ -24,6 +34,13 @@ export default function Categories({
             </TabsTrigger>
           ))}
         </TabsList>
+        <div className="flex md:justify-end md:-mt-[4rem] md:mb-[2rem] p-2 lg:p-0 ">
+          <Input
+            onChange={handleSearch}
+            placeholder="Search"
+            className="md:w-1/3 rounded-xl"
+          />
+        </div>
         {categories.map((category) => (
           <TabsContent key={category} value={category}>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
